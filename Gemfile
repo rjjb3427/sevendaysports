64f9ba3822a4ae8bb030268c397b5f12c1a3dfa6
sirramongabriel <sirramongabriel@gmail.com>
2013-10-20 20:00:37 -0400

    Initial commit
    Forgot tests in last build

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.14'
+gem 'pg'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :tests, :development do
+  gem 'rspec-rails', '~> 2.0'
+  gem 'factory_girl'
+  gem 'pry-rails'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sevendaysports::Application.config.secret_token = '6544867423b757cda8e158cc9c7c9dee4c64ac329487c7b2195c8c043c9f9317cf058633a3ea14d06149af7ee26dca7d82f1c7b8b35d791ee5c65b6d7bd10678'
