9313d5a73419162d46561e24fe31e9b38eeadb57
sirramongabriel <sirramongabriel@gmail.com>
2013-10-22 11:58:00 -0400

    Edit article model
    Decendant of event model and change scope to order by descending

---
+++ app/models/score.rb
@@ -0,0 +1,7 @@
+class Score < Event
+  attr_accessible :author, :away_team_score, :description, :event_id, :home_team_score
+
+  validates_presence_of :author, :description, :home_team_score, :away_team_score
+
+  scope :by_score, -> {where("created_at <= ?", Time.now).order("DESC")}}
+end
