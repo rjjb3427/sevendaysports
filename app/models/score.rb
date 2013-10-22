8fa84b60fbcffe1232cd8f2492b3638f5a947256
sirramongabriel <sirramongabriel@gmail.com>
2013-10-22 12:06:29 -0400

    Edit score model

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,23 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-class Score < ActiveRecord::Base
-=======
+
 class Score < Event
->>>>>>> add-rspec-article-model
-=======
-class Score < Event
->>>>>>> add-rspec-score-model
   attr_accessible :author, :away_team_score, :description, :event_id, :home_team_score
 
   validates_presence_of :author, :description, :home_team_score, :away_team_score
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-  scope :by_score, -> {where("created_at <= ?", Time.now)}}
-=======
   scope :by_score, -> {where("created_at <= ?", Time.now).order("DESC")}}
->>>>>>> add-rspec-article-model
-=======
-  scope :by_score, -> {where("created_at <= ?", Time.now).order("DESC")}}
->>>>>>> add-rspec-score-model
 end
