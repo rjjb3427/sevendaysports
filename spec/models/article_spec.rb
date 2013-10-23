16f3391171e6d21836be47bdd1bfd31e921c0405
sirramongabriel <sirramongabriel@gmail.com>
2013-10-23 15:10:06 -0400

    Demonstrate article_spec no_method_error
    Performed rake db:migrate, rake db:test:prepare, and finally rake db:reset to no avail

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe Article do
-  it "requires a #body" do
+  it "requires a #body to be present" do
     article = FactoryGirl.build :article, body: nil
     expect(article).not_to be_valid
-    article.body = "I am your first post!"
-    expect(article.body).to eq "I am your first post!"
+    # article.body = "I am your first post!"
+    # expect(article).to be_valid
   end
 end
