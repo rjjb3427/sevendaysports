34a411871d553cd672d019ed446eafdab3b143d2
sirramongabriel <sirramongabriel@gmail.com>
2013-10-23 15:24:14 -0400

    Finish media_spec.rb

--- spec/factories/medias.rb
+++ spec/factories/medias.rb
@@ -5,8 +5,5 @@ FactoryGirl.define do
     title "first image"
     type "image"
     url "http://example.com"
-    event_id 1
-    team_id 1
-    user_id 1
   end
 end
--- spec/models/media_spec.rb
+++ spec/models/media_spec.rb
@@ -5,34 +5,34 @@ describe Media do
     media = FactoryGirl.build :media, author: nil
     expect(media).not_to be_valid 
     media.author = "happy gilmore"
-    expect(media.author).to be_present
+    expect(media).to be_valid
   end
 
   it "requires #details to be present" do
     media = FactoryGirl.build :media, details: nil
     expect(media).not_to be_valid
     media.details = "Happy gilmore taking golf swing"
-    expect(media.details).to be_present
+    expect(media).to be_valid
   end
 
   it "requires #title to be present" do 
     media = FactoryGirl.build :media, title: nil
     expect(media).not_to be_valid
     media.title = "Gilmore's first golf swing"
-    expect(media.title).to be_present
+    expect(media).to be_valid
   end
 
   it "requires #type to be present" do
     media = FactoryGirl.build :media, type: nil
     expect(media).not_to be_valid
     media.type = "image"
-    expect(media.type).to be_present
+    expect(media).to be_valid
   end
 
   it "requires #url to be present" do
     media = FactoryGirl.build :media, url: nil
     expect(media).not_to be_valid
     media.url = "http://example.org"
-    expect(media.url).to be_present
+    expect(media).to be_valid
   end
 end
