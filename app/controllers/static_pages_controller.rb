682b458b13181afc1298dd3575677b070a10657a
sirramongabriel <sirramongabriel@gmail.com>
2013-11-29 15:20:04 -0500

    Create recent scope for media
    Goal: query medias from < 4 days ago

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
+    @medias = Media.by_user(user_id)
   end
 
   def contact
--- app/models/media.rb
+++ app/models/media.rb
@@ -9,4 +9,5 @@ class Media < ActiveRecord::Base
   scope :by_team, ->(team_id) { where(team_id: team_id).order(name: name) }
   scope :by_event, ->(event_id) { where(event_id: event_id).order(event_on: event_on) }
   scope :by_media, -> { where(id: id).order(created_at: :created_at) }
+  scope :recent, -> { where(id: id).order(created_at: :created_at).limit(3) }
 end
