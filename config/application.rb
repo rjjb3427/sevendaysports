437de35fd0941cbda36c738d35b569db716f5026
sirramongabriel <sirramongabriel@gmail.com>
2013-12-08 17:40:27 -0500

    Precompile assets for production ENV

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Sevendaysports
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # fix preompiler error: http://goo.gl/7VQrCI
+    config.assets.initialize_on_precompile = false
   end
 end
