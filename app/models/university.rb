56ddc91024d3e12231a5828811425d867a28126d
sirramongabriel <sirramongabriel@gmail.com>
2013-12-08 20:30:40 -0500

    PG relations:error for University.all in static_pages_controller.rb:3

--- app/models/university.rb
+++ app/models/university.rb
@@ -7,7 +7,7 @@
             length: { minimum: 5 }
   validates_presence_of :name, :address, :city, :state, :url
 
-  scope :universities, -> { University.order(name: 'ASC') }
+  #scope :ordered, -> { University.order(name: 'ASC') }
   # scope :by_teams, ->(university_id) { Team.find_by_university_id(university_id) }
   # scope :team_by_university, ->(team_id) { where(team_id: team_id).order(name: name)}
 
